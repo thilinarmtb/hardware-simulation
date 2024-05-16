449c96cbf33f56e8b4a1765dbfb152e34f2618d4
Thilina Ratnayaka <thilinarmtb@gmail.com>
2024-05-16 13:47:46 -0500

    Add change dump (tracing) to blinky

--- 004_blinky/blinky.cpp
+++ 004_blinky/blinky.cpp
@@ -2,34 +2,46 @@
 #include <cstdlib>
 
 #include "verilated.h"
+#include "verilated_vcd_c.h"
 
 #include "Vblinky.h"
 
 // Example found in the tutorial: https://zipcpu.com/tutorial/lsn-01-wires.pdf
 
-static void tick(const std::unique_ptr<Vblinky> &pbky) {
-  pbky->eval();
-  pbky->i_clk = 1;
-  pbky->eval();
-  pbky->i_clk = 0;
-  pbky->eval();
+static void tick(const unsigned tick_count, const std::unique_ptr<Vblinky> &bky,
+                 const std::unique_ptr<VerilatedVcdC> &tfp) {
+  bky->eval();
+  tfp->dump(tick_count * 10 - 2);
+  bky->i_clk = 1;
+  bky->eval();
+  tfp->dump(tick_count * 10);
+  bky->i_clk = 0;
+  bky->eval();
+  tfp->dump(tick_count * 10 + 5);
 }
 
 int main(int argc, char *argv[]) {
-  const std::unique_ptr<VerilatedContext> pctx{new VerilatedContext{}};
-  pctx->commandArgs(argc, argv);
+  const std::unique_ptr<VerilatedContext> ctx{new VerilatedContext{}};
+  ctx->commandArgs(argc, argv);
 
-  const std::unique_ptr<Vblinky> pbky{new Vblinky{pctx.get()}};
+  const std::unique_ptr<Vblinky> bky{new Vblinky{ctx.get()}};
+  ctx->traceEverOn(true);
 
-  int last_led = 1;
+  const std::unique_ptr<VerilatedVcdC> tfp{new VerilatedVcdC};
+  bky->trace(tfp.get(), 00);
+  tfp->open("blinkytrace.vcd");
+
+  int last_led = bky->o_led;
+  unsigned tick_count = 0;
   for (int t = 0; t < (1 << 20); t++) {
-    tick(pbky);
-    if (last_led != pbky->o_led) printf("t = %7d, led = %d\n", t, pbky->o_led);
-    last_led = pbky->o_led;
+    tick_count++;
+    tick(tick_count, bky, tfp);
+    if (last_led != bky->o_led) printf("t = %7d, led = %d\n", t, bky->o_led);
+    last_led = bky->o_led;
   }
 
-  pbky->final();
-  pctx->statsPrintSummary();
+  bky->final();
+  ctx->statsPrintSummary();
 
   return 0;
 }
